dec8456572e8fec14bf13db4b621c18fc5560731
Memitaru <amandatwah@gmail.com>
2019-01-20 13:15:17 -0800

    Completed MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -2,10 +2,21 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 
+
+
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let x=0;
+  return() => (++x);
 };
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
